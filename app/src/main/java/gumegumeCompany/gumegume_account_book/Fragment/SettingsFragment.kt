22de0e40b6a1e9e1c9677f02885264f79dab1725
sunjoolee <sheisthepoem@naver.com>
2023-07-13 12:29:11 +0900

    fragment_settings.xml

--- app/src/main/java/gumegumeCompany/gumegume_account_book/Fragment/SettingsFragment.kt
+++ app/src/main/java/gumegumeCompany/gumegume_account_book/Fragment/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package gumegumeCompany.gumegume_account_book.Fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kakao.sdk.user.UserApiClient
 import gumegumeCompany.gumegume_account_book.databinding.FragmentSettingsBinding
 
 
@@ -21,6 +23,24 @@ class SettingsFragment : Fragment() {
     ): View? {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        //TODO: 사용자 정보 받아와 프로필에 표시하기
+        //사용자 이름
+        //사용자 성별
+        //사용자 생년월일
+
+        binding.logoutBtn.setOnClickListener {
+
+        }
+
+        //문의하기
+        binding.contactChatbotBtn.setOnClickListener {
+
+        }
+
+        binding.contactEmailBtn.setOnClickListener {
+
+        }
 
         return view
     }
