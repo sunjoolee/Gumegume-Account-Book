3e57e8b8278ab891f3378179f5c9a99f1805cbae
sunjoolee <sheisthepoem@naver.com>
2023-07-13 18:19:59 +0900

    settings fragment - user profile

--- app/src/main/java/gumegumeCompany/gumegume_account_book/Fragment/SettingsFragment.kt
+++ app/src/main/java/gumegumeCompany/gumegume_account_book/Fragment/SettingsFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isGone
 import com.kakao.sdk.user.UserApiClient
 import gumegumeCompany.gumegume_account_book.R
 import gumegumeCompany.gumegume_account_book.databinding.FragmentSettingsBinding
@@ -30,18 +31,49 @@ class SettingsFragment : Fragment() {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        //TODO: 사용자 정보 받아와 프로필에 표시하기
-        //사용자 이름
-        //사용자 성별
-        //사용자 생년월일
+        //현재 로그인한 사용자 정보 프로필에 표시
+        var userName:String = "비회원"
+        var userGender:String = "unKnown"
+        var userBirthYear:String = "0000";
+        var userBirthDate:String = "00/00";
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                Log.e(TAG, "사용자 정보 요청 실패", error)
+            }
+            else if (user != null) {
+                Log.i(TAG, "사용자 정보 요청 성공")
+                userName = user.kakaoAccount?.name.toString()
+                userGender = user.kakaoAccount?.gender.toString()
+                userBirthYear = user.kakaoAccount?.birthyear.toString()
+                userBirthDate = user.kakaoAccount?.legalBirthDate.toString()
+            }
+        }
+        binding.profileUserName.text = resources.getString(R.string.user_name, userName)
+        binding.profileUserGender.apply{
+            when(userGender){
+                "unKnown" -> isGone = true
+                "male" -> { isGone = false
+                    setImageDrawable(resources.getDrawable(R.drawable.mars_18)) }
+                "female" -> { isGone = false
+                    setImageDrawable(resources.getDrawable(R.drawable.venus_18)) }
+            }
+        }
+        binding.profileUserBirth.text = resources.getString(R.string.user_birth, userBirthYear, userBirthDate)
 
         binding.logoutBtn.setOnClickListener {
-
+            UserApiClient.instance.logout { error ->
+                if (error != null) {
+                    Log.e(TAG, "로그아웃 실패. SDK에서 토큰 삭제됨", error)
+                }
+                else {
+                    Log.i(TAG, "로그아웃 성공. SDK에서 토큰 삭제됨")
+                }
+            }
         }
 
         //문의하기
         binding.contactChatbotBtn.setOnClickListener {
-
+            //TODO: implement chatbot
         }
 
         binding.contactEmailBtn.setOnClickListener {
