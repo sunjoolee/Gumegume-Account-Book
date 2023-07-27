78ce4a8451d216974fae27ca2f3666e7a69dfa50
sunjoolee <sheisthepoem@naver.com>
2023-07-27 16:25:29 +0900

    fragment_add_account.xml change view ids to snake case

--- app/src/main/java/gumegumeCompany/gumegume_account_book/Fragment/AddAccountFragment.kt
+++ app/src/main/java/gumegumeCompany/gumegume_account_book/Fragment/AddAccountFragment.kt
@@ -23,11 +23,11 @@ import java.util.Calendar
 import java.util.Date
 
 class AddAccountFragment : Fragment() {
-    private var _binding: FragmentAddAccountBinding?= null
+    private var _binding: FragmentAddAccountBinding? = null
     private val binding get() = _binding
 
-    private lateinit var accountType : String //수입 or 지출
-    private lateinit var accountCategoryType:String //하위 카테고리
+    private lateinit var accountType: String //수입 or 지출
+    private lateinit var accountCategoryType: String //하위 카테고리
 
     private var incomeCnt = 0
     private var expensesCnt = 0
@@ -35,19 +35,21 @@ class AddAccountFragment : Fragment() {
     //수입 하위 카테고리 버튼 목록
     private val incomeCategorySelectBtns = binding?.let {
         arrayListOf<TextView>(
-        it.salarySelectBtn, it.allowanceSelectBtn
-    )
+            it.salarySelectBtn,
+            it.allowanceSelectBtn
+        )
     }
+
     //지출 하위 카테고리 버튼 목록
     private val expensesCategorySelectBtns = binding?.let {
         arrayListOf<TextView>(
-        it.fixedExpensesSelectBtn, it.foodSelectBtn, it.dailyNecessitySelectBtn,
+            it.fixedExpensesSelectBtn, it.foodSelectBtn, it.dailyNecessitySelectBtn,
             it.giftSelectBtn, it.etcSelectBtn
-    )
+        )
     }
 
     //하위 카테고리 선택 버튼용 OnCLickListener
-    inner class SelectBtnListener : View.OnClickListener{
+    inner class SelectBtnListener : View.OnClickListener {
         override fun onClick(view: View?) {
             accountCategoryType = (view as Button).text.toString()
         }
@@ -61,34 +63,26 @@ class AddAccountFragment : Fragment() {
         _binding = FragmentAddAccountBinding.inflate(inflater, container, false)
         val view = binding?.root
 
-        //수입 하위 카테고리 버튼 목록
-        incomeCategorySelectBtns = mutableListOf<TextView>(
-            binding.salarySelectBtn, binding.allowanceSelectBtn
-        )
-        //지출 하위 카테고리 버튼 목록
-        expensesCategorySelectBtns = mutableListOf<TextView>(
-            binding.fixedExpensesSelectBtn, binding.foodSelectBtn, binding.dailyNecessitySelectBtn,
-            binding.giftSelectBtn, binding.etcSelectBtn
-        )
-
         //TODO: 화면 회전시 입력 데이터값 유지
 
         //내역 날짜 디폴트 값 설정
         //홈 화면에서 추가하는 경우, 현재 날짜
         var accountDateStr = LocalDate.now()
             .format(DateTimeFormatter.ofPattern("yyyy/MM/dd"))
-        binding?.accountDateBtn?.text  = accountDateStr
-        //TODO: 커스텀 캘린터 일일 내역에서 추가하는 경우, 일일 내역의 날짜
+        binding?.accountDateBtn?.text = accountDateStr
+        //TODO: day detail info fragment에서 추가하는 경우, 일일 내역의 날짜
 
         //날짜 버튼 -> Date Picker Dialog로 선택하기
-        binding?.accountDateBtn?.setOnClickListener{
+        binding?.accountDateBtn?.setOnClickListener {
             val cal = Calendar.getInstance()
-            val dateSetListener = DatePickerDialog.OnDateSetListener{view,year,month,day ->
+            val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, day ->
                 accountDateStr = "${year}/${month}/${day}"
-                binding?.accountDateBtn?.text  = accountDateStr
+                binding?.accountDateBtn?.text = accountDateStr
             }
-            DatePickerDialog(this.requireContext(), dateSetListener,
-                cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH))
+            DatePickerDialog(
+                this.requireContext(), dateSetListener,
+                cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)
+            )
                 .show()
         }
 
@@ -97,7 +91,7 @@ class AddAccountFragment : Fragment() {
 
             // 수입 버튼 클릭 시 색상 변경
             binding!!.incomeBtn.isSelected = binding!!.incomeBtn.isSelected != true
-            if(binding!!.incomeBtn.isSelected == true) binding!!.expensesBtn.isSelected = false
+            if (binding!!.incomeBtn.isSelected == true) binding!!.expensesBtn.isSelected = false
 
             accountType = "income"
             binding?.let { it1 -> showCategories(accountType, it1) }
@@ -107,7 +101,7 @@ class AddAccountFragment : Fragment() {
 
             // 지출 버튼 클릭 시 색상 변경
             binding!!.expensesBtn.isSelected = binding!!.expensesBtn.isSelected != true
-            if(binding!!.expensesBtn.isSelected == true) binding!!.incomeBtn.isSelected = false
+            if (binding!!.expensesBtn.isSelected == true) binding!!.incomeBtn.isSelected = false
 
             accountType = "expenses"
             binding?.let { it1 -> showCategories(accountType, it1) }
@@ -115,12 +109,12 @@ class AddAccountFragment : Fragment() {
 
         //하위 카테고리 onClickListener 지정
         if (incomeCategorySelectBtns != null) {
-            for(btn in incomeCategorySelectBtns){
+            for (btn in incomeCategorySelectBtns) {
                 btn.setOnClickListener(SelectBtnListener())
             }
         }
         if (expensesCategorySelectBtns != null) {
-            for(btn in expensesCategorySelectBtns){
+            for (btn in expensesCategorySelectBtns) {
                 btn.setOnClickListener(SelectBtnListener())
             }
         }
@@ -148,19 +142,19 @@ class AddAccountFragment : Fragment() {
         return view
     }
 
-    private fun showCategories(accountType:String, binding: FragmentAddAccountBinding){
-        var incomeBtnVisibility:Boolean = when(accountType){
+    private fun showCategories(accountType: String, binding: FragmentAddAccountBinding) {
+        var incomeBtnVisibility: Boolean = when (accountType) {
             "income" -> true
             "outcome" -> false
             else -> true
         }
         if (incomeCategorySelectBtns != null) {
-            for(btn in incomeCategorySelectBtns){
+            for (btn in incomeCategorySelectBtns) {
                 btn.isGone = incomeBtnVisibility
             }
         }
         if (expensesCategorySelectBtns != null) {
-            for(btn in expensesCategorySelectBtns){
+            for (btn in expensesCategorySelectBtns) {
                 btn.isGone = !incomeBtnVisibility
             }
         }
